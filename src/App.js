eecaab593f8bda5aeb06c85f90fc2aec970ea567
avrignaud <vrignaud.agathe@gmail.com>
2023-03-13 18:59:50 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from '../src/utils/horoscope.png';
+import './css/App.css';
+import './css/index.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Augure</p>
+        Everything you don't want to know about your future
       </header>
     </div>
   );
